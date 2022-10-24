3d225f5d30fbe6d1c215ff15a1607b96b694eb77
Jeroen Verhoeven <jeroen.verhoeven@dephion.com>
2022-10-24 09:11:18 +0200

    Added wezterm configuration

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+return {
+    font = wezterm.font 'FiraCode Nerd Font',
+    font_size = 14,
+    color_scheme = 'nord',
+    use_fancy_tab_bar = true,
+    window_decorations = "TITLE | RESIZE",
+}
