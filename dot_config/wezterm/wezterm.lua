96d130235f8aeceeac080e3eefb464d80e2defda
Jeroen Verhoeven <jeroen@joentje.org>
2022-10-25 07:37:52 +0200

    Added wezterm configuration

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+return {
+    font = wezterm.font 'FiraCode Nerd Font',
+    font_size = 14,
+    color_scheme = 'nord',
+    use_fancy_tab_bar = true,
+    window_decorations = "TITLE | RESIZE",
+    window_background_opacity = 0.8,
+}
