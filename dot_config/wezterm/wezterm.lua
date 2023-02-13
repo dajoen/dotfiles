d6c567e87c64c7173b276d99692463252e611e2f
Jeroen Verhoeven <jeroen.verhoeven@dephion.com>
2023-02-13 15:18:43 +0100

    Updated wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,5 +4,6 @@ return {
     font_size = 14,
     color_scheme = 'nord',
     use_fancy_tab_bar = true,
-    window_decorations = "TITLE | RESIZE",
+    window_decorations = "RESIZE",
+    window_background_opacity = 0.75,
 }
