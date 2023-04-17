30d22fccee2e0e206006af926d28a9a834ab43ea
Jeroen Verhoeven <jeroen@joentje.org>
2023-04-17 11:28:33 +0200

    Moved on to zim

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,15 +1,27 @@
 local wezterm = require 'wezterm'
 return {
     font = wezterm.font 'FiraCode Nerd Font',
-    font_size = 14,
+    font_size = 16,
     color_scheme = 'nord',
     use_fancy_tab_bar = true,
     window_decorations = "RESIZE",
     window_background_image = '/Users/jeroenverhoeven/.config/wezterm/unix.jpg',
     window_background_image_hsb = {
-        brightness = 0.1,
-        hue = 1.0,
-        saturation = 0.8,
+        brightness = 0.3,
+        hue = 0.7,
+        saturation = 0.3,
     },
     window_background_opacity = 0.90,
+    keys = {
+    {
+      key = 'n',
+      mods = 'SHIFT|CTRL',
+      action = wezterm.action.ToggleFullScreen,
+    },
+    {
+      key = '"',
+      mods = 'CTRL|SHIFT|ALT',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    },
 }
