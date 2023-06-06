0885f0f4a7e8308938c5a79b22fc40c895c0e48b
Jeroen Verhoeven <jve@bct.nl>
2023-06-06 08:36:11 +0200

    Made some more changes for the Ubuntu setup

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,9 +1,17 @@
 local wezterm = require 'wezterm'
-return {
-    font = wezterm.font 'FiraCode Nerd Font',
-    font_size = 14,
-    color_scheme = 'nord',
-    use_fancy_tab_bar = true,
-    window_decorations = "TITLE | RESIZE",
-    window_background_opacity = 0.8,
-}
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.default_prog = { '/home/linuxbrew/.linuxbrew/bin/zsh', '-l' }
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.font_size = 12
+config.color_scheme = 'nord'
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
+
+return config
